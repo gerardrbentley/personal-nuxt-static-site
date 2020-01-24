e65e19a7fccb50fc6a168ee7c31269a634de5aaf
gerardrbentley <gerardrbentley@gmail.com>
2020-01-24 08:04:11 -0800

    temp switch to faim host

---
+++ publish.el
@@ -0,0 +1,76 @@
+;; publish.el --- Publish org-mode project on Gitlab Pages
+;; Author: Rasmus
+;; Edited By: Gerard
+
+;;; Commentary:
+;; This script will convert the org-mode files in this directory into
+;; html.
+
+;;; Code:
+
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-refresh-contents)
+(package-install 'org-plus-contrib)
+(package-install 'htmlize)
+
+(require 'org)
+(require 'ox-publish)
+
+;; setting to nil, avoids "Author: x" at the bottom
+(setq user-full-name nil)
+
+(setq org-export-with-section-numbers nil
+      org-export-with-smart-quotes t
+      org-export-with-toc nil)
+
+(setq org-html-divs '((preamble "header" "top")
+                      (content "main" "content")
+                      (postamble "footer" "postamble"))
+      org-html-container-element "section"
+      org-html-metadata-timestamp-format "%Y-%m-%d"
+      org-html-checkbox-type 'html
+      org-html-html5-fancy t
+      org-html-validation-link nil
+      org-html-doctype "html5")
+
+(defvar cust-blog-header
+  (with-temp-buffer
+    (insert-file-contents "./preamble.html")
+    (buffer-string)))
+
+(defvar site-attachments
+  (regexp-opt '("jpg" "jpeg" "gif" "png" "svg"
+                "ico" "cur" "css" "js" "woff" "html" "pdf"))
+  "File types that are published as static files.")
+
+(setq org-publish-project-alist
+      (list
+       (list "site-org"
+             :base-directory "."
+             :base-extension "org"
+             :recursive t
+             :publishing-function '(org-html-publish-to-html)
+             :publishing-directory "./public"
+             :exclude (regexp-opt '("README" "draft" "header" "preamble"))
+             :auto-sitemap t
+             :sitemap-filename "index.org"
+             :sitemap-title "FAIM Resources Sitemap"
+             :sitemap-file-entry-format "%d *%t*"
+             :html-head-extra "<link rel=\"icon\" type=\"image/x-icon\" href=\"/favicon.ico\"/>"
+             :html-preamble cust-blog-header
+             :sitemap-style 'list
+             :sitemap-sort-files 'chronologically)
+       (list "site-static"
+             :base-directory "."
+             :exclude "public/"
+             :base-extension site-attachments
+             :publishing-directory "./public"
+             :publishing-function 'org-publish-attachment
+             :recursive t)
+       (list "site" :components '("site-org"))))
+
+(provide 'publish)
+;;; publish.el ends here
