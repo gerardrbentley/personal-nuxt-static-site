27f24b2b35a3ba7c84340dab5f2e4d1a29337ba4
gbkh2015 <gbkh2015@mymail.pomona.edu>
2020-03-29 18:02:03 -0700

    skeleton for full site

---
+++ pages/projects/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="blog-page content relative">
+    <div class="w-full h-16 font-bold text-xl bg-dark text-white">
+      COMING SOON
+    </div>
+    <posts-section :posts="posts" />
+  </div>
+</template>
+
+<script>
+import PostsSection from "~/components/Sections/PostsSection";
+import postlist from "~/contents/activeposts.js";
+
+const title = "Gerard Bentley - Lost Crumbs";
+const description = "Full-Stack Development and Daily Computing Blog.";
+
+export default {
+  async asyncData({ app }) {
+    const posts = postlist;
+
+    async function asyncImport(postName) {
+      const wholeMD = await import(`~/contents/posts/${postName}.md`);
+      return wholeMD.attributes;
+    }
+
+    return Promise.all(posts.map((post) => asyncImport(post))).then((res) => {
+      return {
+        posts: res
+      };
+    });
+  },
+
+  components: {
+    PostsSection
+  },
+
+  transition: {
+    name: "slide-fade"
+  },
+
+  head() {
+    return {
+      title: title,
+      meta: [
+        { name: "author", content: "Gerard Bentley" },
+        {
+          name: "description",
+          property: "og:description",
+          content: description,
+          hid: "description"
+        },
+        { property: "og:title", content: title },
+        { property: "og:image", content: this.ogImage },
+        { name: "twitter:description", content: description },
+        { name: "twitter:image", content: this.ogImage }
+      ]
+    };
+  },
+
+  computed: {
+    ogImage: function() {
+      return require("~/assets/images/full_bg.jpg");
+    }
+  }
+};
+</script>
--- pages/resume.vue
+++ pages/resume.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="resumePage">
     <div class="bg-primary-50 h-auto w-screen pt-16 pb-8">
-      
-      <a href="/gbentley_resume.pdf">
-      <button class="absolute right-0 mr-12 mt-4 bg-primary border-2 border-primary-700 rounded-full h-12 w-32 text-white text-xl hover:shadow-outline hover:bg-primary-700 z-30">Download</button>
+      <a download title="/gbentley_resume.pdf" href="/gbentley_resume.pdf">
+        <div
+          class="absolute right-0 mr-12 mt-4 bg-primary border-2 border-primary-700 rounded-full h-12 w-32 text-white text-xl hover:shadow-outline hover:bg-primary-700 z-30 flex items-center justify-center"
+        >
+          Download
+        </div>
       </a>
-      <div class="bg-primary-100 background shadow-2xl border-2 border-primary-700 w-11/12 h-full m-auto">
-      
+      <div
+        class="bg-primary-100 background shadow-2xl border-2 border-primary-700 w-11/12 h-full m-auto"
+      >
         <pdf class="pdf" src="/gbentley_resume.pdf" :page="1"></pdf>
       </div>
     </div>
   </div>
 </template>
 
-<script>
+<script></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>
